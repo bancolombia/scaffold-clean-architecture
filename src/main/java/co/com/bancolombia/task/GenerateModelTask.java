4ee151d73f7f6c53c732ee8005108609e420bd32
Santiago Garcia <santitigaga@hotmail.com>
2019-12-13 11:19:23 -0500

    templates interface and model

--- src/main/java/co/com/bancolombia/task/GenerateModelTask.java
+++ src/main/java/co/com/bancolombia/task/GenerateModelTask.java
@@ -6,50 +6,46 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.options.Option;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 
 public class GenerateModelTask extends DefaultTask {
-    public String nameModel = "";
-    public String _package = "";
+    public String modelName = "";
+    public String _package = "co.com.bancolombia";
 
-    @Option(option = "name", description = "Set the name of the model default is cleanArchitecture ")
-    public void setNameProject(String nameModel) {
-        if (!nameModel.isEmpty()){
-            this.nameModel = nameModel;
+    @Option(option = "name", description = "Set the model name")
+    public void setNameProject(String modelName) {
+        if (!modelName.isEmpty()) {
+            this.modelName = modelName;
         }
     }
 
 
     @TaskAction
-    public void Generate() throws IOException {
-    if(nameModel.isEmpty() || nameModel.isBlank()){
-        System.out.println("Set the name of the model with --name");
-        System.exit(1);
-    }
-        System.out.println("Name Model: " + nameModel);
+    public void GenerateModel() throws IOException {
+        if (modelName.isEmpty() || modelName.isBlank()) {
+            System.out.println("Set the model name with the parameter --name");
+            System.exit(1);
+        }
+        _package = _package.replaceAll("\\.", "\\/");
+        System.out.println("Model Name: " + modelName);
 
         System.out.println("Generating Childs Dirs");
-        getProject().mkdir(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(nameModel)).concat("/").concat(Constants.gateway));
+        getProject().mkdir(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(modelName)).concat("/").concat(Constants.gateway));
         System.out.println("Generated Childs Dirs");
 
         System.out.println("Generating Base Files");
-        getProject().file(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(nameModel).concat("/").concat(Constants.gateway).concat("/").concat(Utils.capitalize(nameModel) + Constants.repository + Constants.extensionJava))).createNewFile();
-        getProject().file(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(nameModel).concat("/").concat(Utils.capitalize(nameModel) + Constants.extensionJava))).createNewFile();
+        getProject().file(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(modelName).concat("/").concat(Constants.gateway).concat("/").concat(Utils.capitalize(modelName) + Constants.repository + Constants.extensionJava))).createNewFile();
+        getProject().file(Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(modelName).concat("/").concat(Utils.capitalize(modelName) + Constants.extensionJava))).createNewFile();
 
 
         System.out.println("Generated Base Files");
         System.out.println("Writing in Files");
-        Utils.writeString(getProject(), Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(nameModel)).concat("/").concat(Constants.gateway).concat("/").concat(Utils.capitalize(nameModel) + Constants.repository + Constants.extensionJava), Constants.getInterfaceModel(nameModel, _package));
-        Utils.writeString(getProject(), Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(nameModel)).concat("/").concat(Utils.capitalize(nameModel) + Constants.extensionJava), Constants.getModel(nameModel,  _package));
+        Utils.writeString(getProject(), Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(modelName)).concat("/").concat(Constants.gateway).concat("/").concat(Utils.capitalize(modelName) + Constants.repository + Constants.extensionJava), Constants.getInterfaceModel(modelName, _package));
+        Utils.writeString(getProject(), Constants.domain.concat("/").concat(Constants.model).concat("/").concat(Constants.mainJava).concat("/").concat(_package).concat("/").concat(Constants.model).concat("/").concat(Utils.decapitalize(modelName)).concat("/").concat(Utils.capitalize(modelName) + Constants.extensionJava), Constants.getModel(modelName, _package));
         System.out.println("Writed in Files");
 
     }
-
-
-
 
 
 }
