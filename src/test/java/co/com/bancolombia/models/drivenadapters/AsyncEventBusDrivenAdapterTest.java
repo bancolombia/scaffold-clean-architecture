d4fe9ed7626eb6d66cd538e86e673be565dd3b92
jestebangl <juan.1701120500@gmail.com>
2020-04-30 10:34:08 -0500

    increasing coverage on driven adapters models

---
+++ src/test/java/co/com/bancolombia/models/drivenadapters/AsyncEventBusDrivenAdapterTest.java
@@ -0,0 +1,29 @@
+package co.com.bancolombia.models.drivenadapters;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class AsyncEventBusDrivenAdapterTest {
+
+    AsyncEventBusDrivenAdapter adapter;
+
+    @Before
+    public void init() throws IOException {
+        adapter = new AsyncEventBusDrivenAdapter();
+    }
+
+    @Test
+    public void getInterfaceModuleTest() {
+        Assert.assertNotNull(adapter.getInterfaceModule());
+    }
+
+    @Test
+    public void getPropertiesFileContentTest() {
+        Assert.assertNull(adapter.getPropertiesFileContent());
+    }
+
+
+}
