ed5a6d347b0ff756eb8166b5f85a6beb31964858
Juan Carlos Galvis Zuluaga <jcgalvis@bancolombia.com.co>
2020-06-16 13:48:15 -0500

    create some unit test

--- src/test/java/co/com/bancolombia/task/GenerateDrivenAdapterTaskTest.java
+++ src/test/java/co/com/bancolombia/task/GenerateDrivenAdapterTaskTest.java
@@ -1,5 +1,6 @@
 package co.com.bancolombia.task;
 
+import co.com.bancolombia.Constants;
 import co.com.bancolombia.exceptions.CleanException;
 import co.com.bancolombia.factory.adapters.ModuleFactoryDrivenAdapter;
 import org.gradle.api.Project;
@@ -9,7 +10,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GenerateDrivenAdapterTaskTest {
@@ -105,6 +108,23 @@ public class GenerateDrivenAdapterTaskTest {
     }
 
     @Test
+    public void generateDrivenAdapterMongoRepositoryWithSecrets() throws IOException, CleanException {
+        // Arrange
+        task.setType(ModuleFactoryDrivenAdapter.DrivenAdapterType.MONGODB);
+        task.setSecret(Constants.BooleanOption.TRUE);
+        // Act
+        task.generateDrivenAdapterTask();
+        // Assert
+        assertTrue(new File("build/unitTest/infrastructure/driven-adapters/mongo-repository/build.gradle").exists());
+        assertTrue(new File("build/unitTest/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/MongoDBRepository.java").exists());
+        assertTrue(new File("build/unitTest/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/MongoRepositoryAdapter.java").exists());
+        assertTrue(new File("build/unitTest/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/helper/AdapterOperations.java").exists());
+        assertTrue(new File("build/unitTest/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/config/MongoDBSecret.java").exists());
+        assertTrue(new File("build/unitTest/applications/app-service/src/main/java/co/com/bancolombia/config/MongoConfig.java").exists());
+        assertTrue(new File("build/unitTest/applications/app-service/src/main/java/co/com/bancolombia/config/SecretsConfig.java").exists());
+    }
+
+    @Test
     public void generateDrivenAdapterEventBus() throws IOException, CleanException {
         // Arrange
         task.setType(ModuleFactoryDrivenAdapter.DrivenAdapterType.ASYNCEVENTBUS);
@@ -114,5 +134,14 @@ public class GenerateDrivenAdapterTaskTest {
         assertTrue(new File("build/unitTest/infrastructure/driven-adapters/async-event-bus/build.gradle").exists());
         assertTrue(new File("build/unitTest/infrastructure/driven-adapters/async-event-bus/src/main/java/co/com/bancolombia/event/ReactiveEventsGateway.java").exists());
         assertTrue(new File("build/unitTest/domain/model/src/main/java/co/com/bancolombia/model/event/gateways/EventsGateway.java").exists());
+    }
+
+    @Test
+    public void shouldGetSecretOptions() {
+        // Arrange
+        // Act
+        List<Constants.BooleanOption> options = task.getSecretOptions();
+        // Assert
+        assertEquals(2, options.size());
     }
 }
