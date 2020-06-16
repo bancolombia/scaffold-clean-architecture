9a73b5870d8c52240b5d490b7af29ccef04308b9
Juan Carlos Galvis Zuluaga <jcgalvis@bancolombia.com.co>
2020-06-16 18:19:11 -0500

    remove unused autogenerated methods

--- src/main/java/co/com/bancolombia/models/TemplateDefinition.java
+++ src/main/java/co/com/bancolombia/models/TemplateDefinition.java
@@ -1,11 +1,15 @@
 package co.com.bancolombia.models;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.Map;
 
-@Data
+@NoArgsConstructor
+@Getter
+@Setter
 public class TemplateDefinition {
     private Map<String, String> files;
     private List<String> folders;
