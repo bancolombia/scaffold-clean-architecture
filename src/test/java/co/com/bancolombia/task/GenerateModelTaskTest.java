cf3b489e0e648062b973812558cb33782d141b69
Juan Carlos Galvis <jcgalvis@bancolombia.com.co>
2020-11-11 14:11:39 -0500

    Fix JPA Driven Adapter Dependencies (#97)
    * #95 fixed
    
    * allow gradle parallel dependencies download #80
    
    * fix unit test and update sonar properties
    
    * increment version
    
    Co-authored-by: Santiago Garcia <sagarci@bancolombia.com.co>

--- src/test/java/co/com/bancolombia/task/GenerateModelTaskTest.java
+++ src/test/java/co/com/bancolombia/task/GenerateModelTaskTest.java
@@ -4,6 +4,7 @@ import co.com.bancolombia.exceptions.CleanException;
 import co.com.bancolombia.exceptions.ParamNotFoundException;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,28 +14,26 @@ import static org.junit.Assert.assertTrue;
 
 
 public class GenerateModelTaskTest {
+    private GenerateModelTask task;
 
 
-    @Test(expected = IllegalArgumentException.class)
-    public void generateEntryPointValueUnExistent() throws IOException, CleanException {
-        Project project = ProjectBuilder.builder().build();
-        project.getTasks().create("test", GenerateModelTask.class);
+    @Before
+    public void setup() throws IOException, CleanException {
+        Project project = ProjectBuilder.builder().withProjectDir(new File("build/unitTest")).build();
         project.getTasks().create("ca", GenerateStructureTask.class);
+        project.getTasks().create("test", GenerateModelTask.class);
         GenerateStructureTask caTask = (GenerateStructureTask) project.getTasks().getByName("ca");
         caTask.generateStructureTask();
+        task = (GenerateModelTask) project.getTasks().getByName("test");
+    }
 
-        GenerateModelTask task = (GenerateModelTask) project.getTasks().getByName("test");
-
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWithoutArgumentsForModel() throws IOException, CleanException {
         task.generateModelTask();
     }
 
     @Test
-    public void generateEntryPoint() throws IOException, ParamNotFoundException {
-        Project project = ProjectBuilder.builder().withProjectDir(new File("build/unitTest")).build();
-        project.getTasks().create("test", GenerateModelTask.class);
-
-        GenerateModelTask task = (GenerateModelTask) project.getTasks().getByName("test");
-
+    public void shouldGenerateModel() throws IOException, ParamNotFoundException {
         task.setName("testModel");
         task.generateModelTask();
         assertTrue(new File("build/unitTest/domain/model/src/main/java/co/com/bancolombia/model/testmodel/gateways/TestModelRepository.java").exists());
