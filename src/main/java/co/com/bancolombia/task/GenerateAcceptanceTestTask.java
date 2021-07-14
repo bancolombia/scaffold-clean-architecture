87563adade849a1cd79ebfa14ecdda42f26e42a3
Ingenieria de Software <sagarci@bancolombia.com.co>
2021-07-14 10:56:02 -0500

    remove logger lifecycle package

---
+++ src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
@@ -0,0 +1,18 @@
+package co.com.bancolombia.task;
+
+import co.com.bancolombia.exceptions.CleanException;
+import co.com.bancolombia.utils.Utils;
+import java.io.IOException;
+import org.gradle.api.tasks.TaskAction;
+
+public class GenerateAcceptanceTestTask extends CleanArchitectureDefaultTask {
+
+  @TaskAction
+  public void generateAcceptanceTestTask() throws IOException, CleanException {
+
+    // ModuleFactory acceptanceTestFactory = ModuleFactoryPipeline.getPipelineFactory();
+    logger.lifecycle("Clean Architecture plugin version: {}", Utils.getVersionPlugin());
+    // acceptanceTestFactory.buildModule(builder);
+    builder.persist();
+  }
+}
