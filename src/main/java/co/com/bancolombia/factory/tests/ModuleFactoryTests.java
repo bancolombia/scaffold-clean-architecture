aab298933974a9ee39b4cf0321270cb0843ff10d
Ingenieria de Software <sagarci@bancolombia.com.co>
2021-07-15 09:09:59 -0500

    fix sonar issues

--- src/main/java/co/com/bancolombia/factory/tests/ModuleFactoryTests.java
+++ src/main/java/co/com/bancolombia/factory/tests/ModuleFactoryTests.java
@@ -1,7 +1,10 @@
 package co.com.bancolombia.factory.tests;
 
 import co.com.bancolombia.factory.ModuleFactory;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ModuleFactoryTests {
   public static ModuleFactory getTestsFactory() {
     return new AcceptanceTest();
--- src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
+++ src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
@@ -1,7 +1,6 @@
 package co.com.bancolombia.task;
 
 import co.com.bancolombia.exceptions.CleanException;
-import co.com.bancolombia.factory.ModuleFactory;
 import co.com.bancolombia.factory.tests.ModuleFactoryTests;
 import co.com.bancolombia.utils.Utils;
 import java.io.IOException;
@@ -12,9 +11,8 @@ public class GenerateAcceptanceTestTask extends CleanArchitectureDefaultTask {
   @TaskAction
   public void generateAcceptanceTestTask() throws IOException, CleanException {
 
-    ModuleFactory acceptanceTestFactory = ModuleFactoryTests.getTestsFactory();
     logger.lifecycle("Clean Architecture plugin version: {}", Utils.getVersionPlugin());
-    acceptanceTestFactory.buildModule(builder);
+    ModuleFactoryTests.getTestsFactory().buildModule(builder);
     builder.persist();
   }
 }
