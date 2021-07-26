77f53739ab528548d3189f3878fbe2e7d7f88b54
Daniel Rico <danielrico.posada@gmail.com>
2021-07-26 09:41:38 -0500

    Add constant in java file

--- src/main/java/co/com/bancolombia/factory/adapters/DrivenAdapterRestConsumer.java
+++ src/main/java/co/com/bancolombia/factory/adapters/DrivenAdapterRestConsumer.java
@@ -8,6 +8,8 @@ import org.gradle.api.logging.Logger;
 
 public class DrivenAdapterRestConsumer implements ModuleFactory {
 
+  public static final String APP_SERVICE = "app-service";
+
   @Override
   public void buildModule(ModuleBuilder builder) throws IOException, CleanException {
     Logger logger = builder.getProject().getLogger();
@@ -15,18 +17,18 @@ public class DrivenAdapterRestConsumer implements ModuleFactory {
       logger.lifecycle("Generating rest-consumer for reactive project");
       builder.setupFromTemplate("driven-adapter/consumer-rest/reactive-rest-consumer");
       builder.appendDependencyToModule(
-          "app-service", "compile 'org.springframework.boot:spring-boot-starter-webflux'");
+              APP_SERVICE, "compile 'org.springframework.boot:spring-boot-starter-webflux'");
       builder.appendToProperties("adapter.restconsumer").put("timeout", 5000);
     } else {
       logger.lifecycle("Generating rest-consumer for imperative project");
       builder.setupFromTemplate("driven-adapter/consumer-rest/rest-consumer");
       builder.appendDependencyToModule(
-          "app-service", "compile 'com.fasterxml.jackson.core:jackson-databind'");
+              APP_SERVICE, "compile 'com.fasterxml.jackson.core:jackson-databind'");
     }
     builder
         .appendToProperties("adapter.restconsumer")
         .put("url", builder.getStringParam("task-param-url"));
     builder.appendToSettings("rest-consumer", "infrastructure/driven-adapters");
-    builder.appendDependencyToModule("app-service", "implementation project(':rest-consumer')");
+    builder.appendDependencyToModule(APP_SERVICE, "implementation project(':rest-consumer')");
   }
 }
