05e3ede63c5be357d375f8edc2ca8a14e2f25cfb
Juan Carlos Galvis <juanc.galvis@outlook.com>
2021-10-05 19:40:34 -0500

    upgrade reactive commons dependency version (#167)
    * upgrade reactive commons dependency version

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
   public static final String LOMBOK_VERSION = "1.18.16";
   public static final String JACOCO_VERSION = "0.8.5";
   public static final String COBERTURA_VERSION = "3.0.0";
-  public static final String RCOMMONS_ASYNC_COMMONS_STARTER_VERSION = "1.0.0-beta5";
+  public static final String RCOMMONS_ASYNC_COMMONS_STARTER_VERSION = "1.0.3";
   public static final String RCOMMONS_OBJECT_MAPPER_VERSION = "0.1.0";
   public static final String PLUGIN_VERSION = "1.9.8";
   public static final String GRADLE_WRAPPER_VERSION = "6.7";
