a75e9e80d089644d74bf9d6d6fba22dda8cfe003
JuanC Galvis <8420868+juancgalvis@users.noreply.github.com>
2022-06-03 15:34:25 -0500

    create sqs sender for imperative projects (#253)
    create sqs sender for imperative projects

--- src/main/java/co/com/bancolombia/factory/adapters/DrivenAdapterSQS.java
+++ src/main/java/co/com/bancolombia/factory/adapters/DrivenAdapterSQS.java
@@ -7,21 +7,24 @@ import co.com.bancolombia.exceptions.CleanException;
 import co.com.bancolombia.factory.ModuleBuilder;
 import co.com.bancolombia.factory.ModuleFactory;
 import co.com.bancolombia.factory.commons.GenericModule;
-import co.com.bancolombia.factory.validations.ReactiveTypeValidation;
 import java.io.IOException;
 
 public class DrivenAdapterSQS implements ModuleFactory {
 
   @Override
   public void buildModule(ModuleBuilder builder) throws IOException, CleanException {
-    builder.runValidations(ReactiveTypeValidation.class);
     GenericModule.addAwsBom(builder);
-    builder.setupFromTemplate("driven-adapter/sqs");
+    String template = "driven-adapter/sqs";
+    if (builder.isReactive()) {
+      template = "driven-adapter/sqs-reactive";
+    }
+    builder.setupFromTemplate(template);
     builder.appendToSettings("sqs-sender", "infrastructure/driven-adapters");
     builder
         .appendToProperties("adapter.sqs")
         .put("region", "us-east-1")
-        .put("queueUrl", "http://localhost:4566/000000000000/sample");
+        .put("queueUrl", "http://localhost:4566/000000000000/sample")
+        .put("endpoint", "http://localhost:4566 # For localstack only");
     String dependency = buildImplementationFromProject(builder.isKotlin(), ":sqs-sender");
     builder.appendDependencyToModule(APP_SERVICE, dependency);
   }
