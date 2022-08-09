97e6c63424d04931a7e4c54e3326ce4eb08ec46e
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2022-08-08 19:13:06 -0500

    update reports settings, preparing for compatibility with next gradle 8

---
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2022M08D08.java
@@ -0,0 +1,31 @@
+package co.com.bancolombia.factory.upgrades.actions;
+
+import co.com.bancolombia.Constants;
+import co.com.bancolombia.factory.ModuleBuilder;
+import co.com.bancolombia.factory.upgrades.UpgradeAction;
+import lombok.SneakyThrows;
+
+public class UpgradeY2022M08D08 implements UpgradeAction {
+
+  @Override
+  @SneakyThrows
+  public boolean up(ModuleBuilder builder) {
+    builder.updateExpression(
+        Constants.MainFiles.MAIN_GRADLE, "reportsDir\\s?=", "reportsDirectory =");
+    builder.updateExpression(
+        Constants.MainFiles.MAIN_GRADLE, "(xml|csv|html)\\.enabled", "$1.setRequired");
+    builder.updateExpression(
+        Constants.MainFiles.MAIN_GRADLE, "(xml|csv|html)\\.destination", "$1.setOutputLocation");
+    return true;
+  }
+
+  @Override
+  public String name() {
+    return "2.4.3->2.4.4";
+  }
+
+  @Override
+  public String description() {
+    return "Upgrade jacoco reports config compatible with gradle 8";
+  }
+}
