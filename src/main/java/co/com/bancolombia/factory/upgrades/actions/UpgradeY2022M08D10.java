62ed71c3c49db3317ecb297ac84013945d0a5ea0
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2022-08-11 10:11:20 -0500

    upgrade action

--- src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2022M08D10.java
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2022M08D10.java
@@ -1,32 +1,50 @@
 package co.com.bancolombia.factory.upgrades.actions;
 
-import co.com.bancolombia.factory.ModuleBuilder;
-import co.com.bancolombia.factory.upgrades.UpdateUtils;
-import co.com.bancolombia.factory.upgrades.UpgradeAction;
-import lombok.SneakyThrows;
-
 import static co.com.bancolombia.Constants.MainFiles.MAIN_GRADLE;
 
+import co.com.bancolombia.factory.ModuleBuilder;
+import co.com.bancolombia.factory.upgrades.UpgradeAction;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.SneakyThrows;
+
 public class UpgradeY2022M08D10 implements UpgradeAction {
-    public static final String JUNIT_PLATFORM = "useJUnitPlatform";
-    public static final String SOURCE_COMPATIBILITY = "sourceCompatibility";
 
-    public static final String JUNIT_PLATFORM_VALUER = "\n    test {\n        useJUnitPlatform()\n    } sourceCompatibility ";
+  public static final String VALIDATION = "useJUnitPlatform";
 
-    @Override
-    @SneakyThrows
-    public boolean up(ModuleBuilder builder) {
-        return !UpdateUtils.contains(builder, MAIN_GRADLE, JUNIT_PLATFORM) || UpdateUtils.appendIfNotContains(
-                builder, MAIN_GRADLE, SOURCE_COMPATIBILITY, JUNIT_PLATFORM_VALUER);
+  public static final String JUNIT_PLATFORM_VALUER =
+      "\n    test {\n" + "        useJUnitPlatform()\n" + "    }\n";
+
+  @Override
+  @SneakyThrows
+  public boolean up(ModuleBuilder builder) {
+    AtomicBoolean applied = new AtomicBoolean(false);
+    builder.updateFile(
+        MAIN_GRADLE,
+        content -> {
+          String res = appendValidate(content);
+          if (!content.equals(res)) {
+            applied.set(true);
+          }
+          return res;
+        });
+    return applied.get();
+  }
+
+  @Override
+  public String name() {
+    return "2.4.4->2.4.5";
+  }
+
+  @Override
+  public String description() {
+    return "Append useJUnitPlatform in main.gradle file";
+  }
+
+  private String appendValidate(String main) {
+    if (main.contains(VALIDATION)) {
+      return main;
     }
-
-    @Override
-    public String name() {
-        return "2.4.4->2.4.5";
-    }
-
-    @Override
-    public String description() {
-        return "Append useJUnitPlatform in main.gradle file";
-    }
+    int start = main.indexOf("\tdependencies");
+    return main.substring(0, start) + JUNIT_PLATFORM_VALUER + "\n" + main.substring(start);
+  }
 }
