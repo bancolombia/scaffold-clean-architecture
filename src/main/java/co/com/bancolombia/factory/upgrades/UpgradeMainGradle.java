71df1159375a3d9e8f93045eb1a9dbc5069a8967
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2022-09-22 10:14:15 -0500

    fix unit tests

--- src/main/java/co/com/bancolombia/factory/upgrades/UpgradeMainGradle.java
+++ src/main/java/co/com/bancolombia/factory/upgrades/UpgradeMainGradle.java
@@ -1,28 +1,25 @@
 package co.com.bancolombia.factory.upgrades;
 
-import co.com.bancolombia.factory.ModuleBuilder;
-import lombok.SneakyThrows;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import static co.com.bancolombia.Constants.MainFiles.MAIN_GRADLE;
+
+import co.com.bancolombia.factory.ModuleBuilder;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.SneakyThrows;
 
 public class UpgradeMainGradle {
 
-    @SneakyThrows
-    public boolean up(ModuleBuilder builder, String match, String validation, String value) {
-        AtomicBoolean applied = new AtomicBoolean(false);
-        builder.updateFile(
-                MAIN_GRADLE,
-                content -> {
-                    String res = UpdateUtils.appendValidate(content, match, validation, value);
-                    if (!content.equals(res)) {
-                        applied.set(true);
-                    }
-                    return res;
-                });
-        return applied.get();
-    }
-
-
+  @SneakyThrows
+  public boolean up(ModuleBuilder builder, String match, String validation, String value) {
+    AtomicBoolean applied = new AtomicBoolean(false);
+    builder.updateFile(
+        MAIN_GRADLE,
+        content -> {
+          String res = UpdateUtils.appendValidate(content, match, validation, value);
+          if (!content.equals(res)) {
+            applied.set(true);
+          }
+          return res;
+        });
+    return applied.get();
+  }
 }
