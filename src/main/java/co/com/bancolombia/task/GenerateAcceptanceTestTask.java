38fb02462ab6095a608fbab477a6a549b2e4fa47
Genaro López <glvelez@bancolombia.com.co>
2022-12-20 23:25:44 -0500

    Update rootProjectName in setting.gradle gat template

--- src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
+++ src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
@@ -1,8 +1,8 @@
 package co.com.bancolombia.task;
 
 import co.com.bancolombia.exceptions.CleanException;
-import co.com.bancolombia.factory.tests.ModuleFactoryTests;
 import co.com.bancolombia.factory.ModuleFactory;
+import co.com.bancolombia.factory.tests.ModuleFactoryTests;
 import co.com.bancolombia.utils.Utils;
 import java.io.IOException;
 import org.gradle.api.tasks.TaskAction;
