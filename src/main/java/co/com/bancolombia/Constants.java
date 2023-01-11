1ac69b4f8ca5a914fc0dc840083d8af59fe60a2a
JuanC Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-01-11 17:22:15 -0500

    update dependencies (#298)

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
   public static final String PLUGIN_TASK_GROUP = "Clean Architecture";
   public static final String APP_SERVICE = "app-service";
   public static final String PATH_GRAPHQL = "/graphqlpath";
-  public static final String SECRETS_VERSION = "3.1.0";
-  public static final String SPRING_BOOT_VERSION = "2.7.3";
+  public static final String SECRETS_VERSION = "3.2.0";
+  public static final String SPRING_BOOT_VERSION = "2.7.6";
   public static final String SONAR_VERSION = "3.0";
   public static final String LOMBOK_VERSION = "1.18.24";
   public static final String JACOCO_VERSION = "0.8.8";
@@ -19,8 +19,8 @@ public class Constants {
   public static final String PLUGIN_VERSION = "2.4.6";
   public static final String GRADLE_WRAPPER_VERSION = "7.4.2";
   public static final String KOTLIN_VERSION = "1.6.10";
-  public static final String AWS_BOM_VERSION = "2.17.276";
-  public static final String COMMONS_JMS_VERSION = "0.4.0";
+  public static final String AWS_BOM_VERSION = "2.18.39";
+  public static final String COMMONS_JMS_VERSION = "0.6.0";
   public static final String TOMCAT_EXCLUSION_KOTLIN =
       "configurations {\n"
           + "\tall {\n"
