892e75f92fd800fa279efb52f7b40a81d4097f66
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-01-12 09:33:33 -0500

    add pluginManagement closes #293

---
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2023M01D12.java
@@ -0,0 +1,48 @@
+package co.com.bancolombia.factory.upgrades.actions;
+
+import static co.com.bancolombia.Constants.MainFiles.SETTINGS_GRADLE;
+
+import co.com.bancolombia.factory.ModuleBuilder;
+import co.com.bancolombia.factory.upgrades.UpdateUtils;
+import co.com.bancolombia.factory.upgrades.UpgradeAction;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.SneakyThrows;
+
+public class UpgradeY2023M01D12 implements UpgradeAction {
+  private static final String VALIDATION = "pluginManagement";
+  private static final String VALUE =
+      "pluginManagement {\n"
+          + "    repositories {\n"
+          + "        //mavenLocal()\n"
+          + "        //maven { url '...' }\n"
+          + "        gradlePluginPortal()\n"
+          + "    }\n"
+          + "}\n\n";
+  private static final String MATCH = "";
+
+  @Override
+  @SneakyThrows
+  public boolean up(ModuleBuilder builder) {
+    AtomicBoolean applied = new AtomicBoolean(false);
+    builder.updateFile(
+        SETTINGS_GRADLE,
+        content -> {
+          String res = UpdateUtils.appendValidate(content, MATCH, VALIDATION, VALUE);
+          if (!content.equals(res)) {
+            applied.set(true);
+          }
+          return res;
+        });
+    return applied.get();
+  }
+
+  @Override
+  public String name() {
+    return "2.4.5->2.4.6";
+  }
+
+  @Override
+  public String description() {
+    return "Add pluginManagement in settings.gradle file";
+  }
+}
---
+++ src/test/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2023M01D12Test.java
@@ -0,0 +1,60 @@
+package co.com.bancolombia.factory.upgrades.actions;
+
+import static org.gradle.internal.impldep.org.testng.Assert.assertNotNull;
+import static org.gradle.internal.impldep.org.testng.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import co.com.bancolombia.Constants;
+import co.com.bancolombia.factory.ModuleBuilder;
+import co.com.bancolombia.factory.upgrades.UpgradeAction;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UpgradeY2023M01D12Test {
+  @Mock private Project project;
+  @Mock private Logger logger;
+  private ModuleBuilder builder;
+  private UpgradeAction updater;
+
+  @Before
+  public void setup() throws IOException {
+    when(project.getName()).thenReturn("UtilsTest");
+    when(project.getLogger()).thenReturn(logger);
+    when(project.getProjectDir()).thenReturn(Files.createTempDirectory("sample").toFile());
+    builder = spy(new ModuleBuilder(project));
+    updater = new UpgradeY2023M01D12();
+    assertNotNull(updater.name());
+    assertNotNull(updater.description());
+  }
+
+  @Test
+  public void shouldApplyUpdate() throws IOException {
+    // Arrange
+    builder.addFile(
+        Constants.MainFiles.SETTINGS_GRADLE, readResourceFile("settings-gradle-sample/before.txt"));
+    // Act
+    boolean applied = updater.up(builder);
+    // Assert
+    assertTrue(applied);
+    verify(builder, times(1))
+        .addFile(
+            Constants.MainFiles.SETTINGS_GRADLE,
+            readResourceFile("settings-gradle-sample/after.txt"));
+  }
+
+  private String readResourceFile(String location) throws IOException {
+    ClassLoader classLoader = getClass().getClassLoader();
+    return Files.readString(
+        Path.of(Objects.requireNonNull(classLoader.getResource(location)).getPath()));
+  }
+}
