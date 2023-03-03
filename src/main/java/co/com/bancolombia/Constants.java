c14c790ddd7ea7eb670594ecf2e4adb23874851c
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-03-03 10:07:30 -0500

    start generated code analysis

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String RCOMMONS_ASYNC_COMMONS_STARTER_VERSION = "2.0.0";
   public static final String RCOMMONS_OBJECT_MAPPER_VERSION = "0.1.0";
-  public static final String PLUGIN_VERSION = "3.0.0";
+  public static final String PLUGIN_VERSION = "3.0.0-example";
   public static final String GRADLE_WRAPPER_VERSION = "7.6";
   public static final String KOTLIN_VERSION = "1.6.10";
   public static final String AWS_BOM_VERSION = "2.19.31";
