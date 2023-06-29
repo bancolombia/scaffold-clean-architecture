be4f14a508f5407c8ba9d18f1a8bdbfc2ad6c31b
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-06-29 15:57:03 -0500

    update springboot

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -8,22 +8,27 @@ public class Constants {
   public static final String PLUGIN_TASK_GROUP = "Clean Architecture";
   public static final String APP_SERVICE = "app-service";
   public static final String PATH_GRAPHQL = "/graphql";
-  public static final String SECRETS_VERSION = "4.0.0";
-  public static final String SPRING_BOOT_VERSION = "3.0.6";
-  public static final String SONAR_VERSION = "4.0.0.2929";
-  public static final String LOMBOK_VERSION = "1.18.26";
-  public static final String JACOCO_VERSION = "0.8.8";
+  public static final String SECRETS_VERSION = "4.1.0";
+  public static final String SPRING_BOOT_VERSION = "3.1.1";
+  public static final String SONAR_VERSION = "4.2.1.3168";
+  public static final String LOMBOK_VERSION = "1.18.28";
+  public static final String JACOCO_VERSION = "0.8.10";
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String RCOMMONS_ASYNC_COMMONS_STARTER_VERSION = "2.0.0";
   public static final String RCOMMONS_OBJECT_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.8.RELEASE";
   public static final String PLUGIN_VERSION = "3.3.1";
-  public static final String GRADLE_WRAPPER_VERSION = "7.6.1";
+  public static final String GRADLE_WRAPPER_VERSION = "8.1.1";
   public static final String KOTLIN_VERSION = "1.6.10";
-  public static final String AWS_BOM_VERSION = "2.19.33";
-  public static final String COMMONS_JMS_VERSION = "1.0.0";
+  public static final String AWS_BOM_VERSION = "2.20.94";
+  public static final String COMMONS_JMS_VERSION = "1.1.0";
   public static final String GRAPHQL_KICKSTART_VERSION = "15.0.0";
   public static final String ARCH_UNIT_VERSION = "1.0.1";
+  public static final String OKHTTP_VERSION = "4.11.0";
+  public static final String RESILIENCE_4J_VERSION = "2.0.2";
+  public static final String BIN_STASH_VERSION = "1.2.0";
+  public static final String SPRING_FOX_VERSION = "3.0.0";
+  public static final String DEPENDENCY_CHECK_VERSION = "8.2.1";
   public static final String TOMCAT_EXCLUSION_KOTLIN =
       "configurations {\n"
           + "\tall {\n"
