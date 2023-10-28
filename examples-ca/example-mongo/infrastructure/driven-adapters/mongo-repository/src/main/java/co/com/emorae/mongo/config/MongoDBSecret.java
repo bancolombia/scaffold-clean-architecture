3c07f19b50fddfccfdf70b7e3824a040a54e6838
luisgomez29 <luisgaleano002@gmail.com>
2023-10-28 17:21:55 -0500

    update ms example-mongo example ca

--- examples-ca/example-mongo/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/emorae/mongo/config/MongoDBSecret.java
+++ examples-ca/example-mongo/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/emorae/mongo/config/MongoDBSecret.java
@@ -2,10 +2,9 @@ package co.com.emorae.mongo.config;
 
 import lombok.Builder;
 import lombok.Getter;
+
 @Builder
 @Getter
 public class MongoDBSecret {
-private final String uri;
-
-
+    private final String uri;
 }
