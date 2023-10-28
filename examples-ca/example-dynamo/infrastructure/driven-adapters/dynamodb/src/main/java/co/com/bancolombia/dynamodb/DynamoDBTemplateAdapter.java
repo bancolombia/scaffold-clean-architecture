f51d6a1cecb2ddf8a1400676df3f8235e689920e
luisgomez29 <luisgaleano002@gmail.com>
2023-10-28 16:20:59 -0500

    update ms example-dynamo example ca

--- examples-ca/example-dynamo/infrastructure/driven-adapters/dynamodb/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
+++ examples-ca/example-dynamo/infrastructure/driven-adapters/dynamodb/src/main/java/co/com/bancolombia/dynamodb/DynamoDBTemplateAdapter.java
@@ -2,19 +2,19 @@ package co.com.bancolombia.dynamodb;
 
 import co.com.bancolombia.dynamodb.helper.TemplateAdapterOperations;
 import co.com.bancolombia.model.customer.Customer;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 
 @Repository
 public class DynamoDBTemplateAdapter extends TemplateAdapterOperations<Customer, String, co.com.bancolombia.dynamodb.models.Customer> {
 
-    public DynamoDBTemplateAdapter(AmazonDynamoDB connectionFactory, ObjectMapper mapper) {
+    public DynamoDBTemplateAdapter(DynamoDbEnhancedClient connectionFactory, ObjectMapper mapper) {
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
-        super(connectionFactory, mapper, d -> mapper.map(d, Customer.class));
+        super(connectionFactory, mapper, d -> mapper.map(d, Customer.class), "customer");
     }
 }
