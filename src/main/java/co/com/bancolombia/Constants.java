2b86053a99ed78e49028488929f650eaef210793
luisgomez29 <luisgaleano002@gmail.com>
2023-11-05 17:55:48 -0500

    Update to Gradle 8.4

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
   public static final String RCOMMONS_OBJECT_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.8.RELEASE";
   public static final String PLUGIN_VERSION = "3.6.3";
-  public static final String GRADLE_WRAPPER_VERSION = "8.2.1";
+  public static final String GRADLE_WRAPPER_VERSION = "8.4";
   public static final String KOTLIN_VERSION = "1.6.10";
   public static final String AWS_BOM_VERSION = "2.20.94";
   public static final String COMMONS_JMS_VERSION = "1.2.0";
