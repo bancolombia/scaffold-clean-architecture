c9862fac23e14772fe473a88ff2b53c02525ee9d
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2023-12-07 10:05:28 -0500

    feat(kafka): Add reactive kafka consumer, update archunit and resilience

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
   public static final String GRAPHQL_KICKSTART_VERSION = "15.0.0";
   public static final String ARCH_UNIT_VERSION = "1.2.1";
   public static final String OKHTTP_VERSION = "4.11.0";
-  public static final String RESILIENCE_4J_VERSION = "2.0.2";
+  public static final String RESILIENCE_4J_VERSION = "2.1.0";
   public static final String BIN_STASH_VERSION = "1.2.0";
   public static final String SPRINGDOC_OPENAPI_VERSION = "2.2.0";
   public static final String DEPENDENCY_CHECK_VERSION = "9.0.2";
