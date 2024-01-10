6aeffb0ae83783eeb8ff0723631c0ede4a096ad0
JuanC Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-01-10 10:22:00 -0500

    fix(sonar): Update deprecated sonarqube task to sonar (#418)

---
+++ src/test/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2024M01D10SonarTest.java
@@ -0,0 +1,62 @@
+package co.com.bancolombia.factory.upgrades.actions;
+
+import static co.com.bancolombia.Constants.MainFiles.BUILD_GRADLE;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import co.com.bancolombia.factory.ModuleBuilder;
+import co.com.bancolombia.factory.upgrades.UpgradeAction;
+import co.com.bancolombia.utils.FileUtils;
+import com.github.mustachejava.resolver.DefaultResolver;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UpgradeY2024M01D10SonarTest {
+
+  @Mock private Project project;
+  @Mock private Logger logger;
+
+  private ModuleBuilder builder;
+  private UpgradeAction updater;
+
+  @Before
+  public void setup() throws IOException {
+    when(project.getName()).thenReturn("UtilsTest");
+    when(project.getLogger()).thenReturn(logger);
+    when(project.getProjectDir()).thenReturn(Files.createTempDirectory("sample").toFile());
+
+    builder = spy(new ModuleBuilder(project));
+    updater = new UpgradeY2024M01D10Sonar();
+
+    assertNotNull(updater.name());
+    assertNotNull(updater.description());
+  }
+
+  @Test
+  public void shouldApplyUpdate() throws IOException {
+    DefaultResolver resolver = new DefaultResolver();
+    // Arrange
+    builder.addFile(
+        BUILD_GRADLE,
+        FileUtils.getResourceAsString(resolver, "sonarqube-2-sonar/sonarqube-task-before.txt"));
+    // Act
+    boolean applied = updater.up(builder);
+    // Assert
+    assertTrue(applied);
+    verify(builder)
+        .addFile(
+            BUILD_GRADLE,
+            FileUtils.getResourceAsString(resolver, "sonarqube-2-sonar/sonarqube-task-after.txt"));
+  }
+}
