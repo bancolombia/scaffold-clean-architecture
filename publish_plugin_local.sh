fc591075be7e2ef736a002901dad018e029396d8
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-01-15 14:39:25 -0500

    Revert script file

--- publish_plugin_local.sh
+++ publish_plugin_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-gsed -i 's/version=.*/version=CURRENT_LOCAL_VERSION/g' gradle.properties
-gsed -i 's/PLUGIN_VERSION = .*/PLUGIN_VERSION = "CURRENT_LOCAL_VERSION";/g' src/main/java/co/com/bancolombia/Constants.java
+sed -i 's/version=.*/version=CURRENT_LOCAL_VERSION/g' gradle.properties
+sed -i 's/PLUGIN_VERSION = .*/PLUGIN_VERSION = "CURRENT_LOCAL_VERSION";/g' src/main/java/co/com/bancolombia/Constants.java
 ./gradlew publishToMavenLocal
