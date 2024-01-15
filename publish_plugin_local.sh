e4722123bc2de1d69ac2c3b89cbeabb6a82f36cd
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-01-15 14:26:39 -0500

    Sort adapters by name, update pr template, fix version when generating projects and dependency check task

--- publish_plugin_local.sh
+++ publish_plugin_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-sed -i 's/version=.*/version=3.0.0-example/g' gradle.properties
-sed -i 's/PLUGIN_VERSION = .*/PLUGIN_VERSION = "3.0.0-example";/g' src/main/java/co/com/bancolombia/Constants.java
+gsed -i 's/version=.*/version=CURRENT_LOCAL_VERSION/g' gradle.properties
+gsed -i 's/PLUGIN_VERSION = .*/PLUGIN_VERSION = "CURRENT_LOCAL_VERSION";/g' src/main/java/co/com/bancolombia/Constants.java
 ./gradlew publishToMavenLocal
