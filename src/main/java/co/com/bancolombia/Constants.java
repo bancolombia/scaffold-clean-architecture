99b674408b59dee691f7c07ca5f1068f4d232c79
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-04-22 16:46:05 -0500

    fix(deps): Update dependencies, add semantic release tasks to use github app token

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -10,28 +10,28 @@ public final class Constants {
   public static final String APP_SERVICE = "app-service";
   public static final String PATH_GRAPHQL = "/graphql";
   // dependencies
-  public static final String SECRETS_VERSION = "4.4.0";
-  public static final String SPRING_BOOT_VERSION = "3.2.2";
-  public static final String LOMBOK_VERSION = "1.18.30";
-  public static final String REACTIVE_COMMONS_VERSION = "2.0.4";
+  public static final String SECRETS_VERSION = "4.4.1";
+  public static final String SPRING_BOOT_VERSION = "3.2.5";
+  public static final String LOMBOK_VERSION = "1.18.32";
+  public static final String REACTIVE_COMMONS_VERSION = "2.1.1";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
-  public static final String BLOCK_HOUND_VERSION = "1.0.8.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.23.8";
-  public static final String COMMONS_JMS_VERSION = "2.0.1";
+  public static final String BLOCK_HOUND_VERSION = "1.0.9.RELEASE";
+  public static final String AWS_BOM_VERSION = "2.25.35";
+  public static final String COMMONS_JMS_VERSION = "2.2.2";
   public static final String GRAPHQL_KICKSTART_VERSION = "15.1.0";
-  public static final String ARCH_UNIT_VERSION = "1.2.1";
+  public static final String ARCH_UNIT_VERSION = "1.3.0";
   public static final String OKHTTP_VERSION = "4.12.0";
   public static final String RESILIENCE_4J_VERSION = "2.2.0";
-  public static final String BIN_STASH_VERSION = "1.2.2";
-  public static final String SPRING_DOC_OPENAPI_VERSION = "2.3.0";
+  public static final String BIN_STASH_VERSION = "1.2.4";
+  public static final String SPRING_DOC_OPENAPI_VERSION = "2.5.0";
   // gradle plugins
-  public static final String JACOCO_VERSION = "0.8.11";
-  public static final String SONAR_VERSION = "4.4.1.3373";
+  public static final String JACOCO_VERSION = "0.8.12";
+  public static final String SONAR_VERSION = "5.0.0.4638";
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String PLUGIN_VERSION = "3.16.0";
-  public static final String DEPENDENCY_CHECK_VERSION = "9.0.9";
+  public static final String DEPENDENCY_CHECK_VERSION = "9.1.0";
   // custom
-  public static final String GRADLE_WRAPPER_VERSION = "8.5";
+  public static final String GRADLE_WRAPPER_VERSION = "8.7";
   public static final String KOTLIN_VERSION = "1.9.20";
 
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
