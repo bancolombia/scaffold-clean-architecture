eaaba0b26687c13b96657338048778f4a06896b5
JuanC Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-05-06 08:35:50 -0500

    fix(deps): Update deps (#443)

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -10,19 +10,19 @@ public final class Constants {
   public static final String APP_SERVICE = "app-service";
   public static final String PATH_GRAPHQL = "/graphql";
   // dependencies
-  public static final String SECRETS_VERSION = "4.4.1";
+  public static final String SECRETS_VERSION = "4.4.2";
   public static final String SPRING_BOOT_VERSION = "3.2.5";
   public static final String LOMBOK_VERSION = "1.18.32";
-  public static final String REACTIVE_COMMONS_VERSION = "2.1.1";
+  public static final String REACTIVE_COMMONS_VERSION = "2.2.1";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.9.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.25.35";
-  public static final String COMMONS_JMS_VERSION = "2.2.2";
+  public static final String AWS_BOM_VERSION = "2.25.45";
+  public static final String COMMONS_JMS_VERSION = "2.2.3";
   public static final String GRAPHQL_KICKSTART_VERSION = "15.1.0";
-  public static final String ARCH_UNIT_VERSION = "1.3.0";
+  public static final String ARCH_UNIT_VERSION = "1.1.1";
   public static final String OKHTTP_VERSION = "4.12.0";
   public static final String RESILIENCE_4J_VERSION = "2.2.0";
-  public static final String BIN_STASH_VERSION = "1.2.4";
+  public static final String BIN_STASH_VERSION = "1.2.5";
   public static final String SPRING_DOC_OPENAPI_VERSION = "2.5.0";
   // gradle plugins
   public static final String JACOCO_VERSION = "0.8.12";
