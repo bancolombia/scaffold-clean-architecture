867fc78647d9a83f55a19790f44efe73196dc358
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2024-07-29 12:06:36 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -10,13 +10,13 @@ public final class Constants {
   public static final String APP_SERVICE = "app-service";
   public static final String PATH_GRAPHQL = "/graphql";
   // dependencies
-  public static final String SECRETS_VERSION = "4.4.14";
+  public static final String SECRETS_VERSION = "4.4.15";
   public static final String SPRING_BOOT_VERSION = "3.3.2";
   public static final String LOMBOK_VERSION = "1.18.34";
-  public static final String REACTIVE_COMMONS_VERSION = "4.1.3";
+  public static final String REACTIVE_COMMONS_VERSION = "4.1.4";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.9.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.26.21";
+  public static final String AWS_BOM_VERSION = "2.26.25";
   public static final String COMMONS_JMS_VERSION = "2.2.14";
   public static final String GRAPHQL_KICKSTART_VERSION = "15.1.0";
   public static final String ARCH_UNIT_VERSION = "1.1.1";
