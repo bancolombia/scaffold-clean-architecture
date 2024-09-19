fe0e08f6547f5463f8408b788f1400d319d4255b
JuanC Galvis <8420868+juancgalvis@users.noreply.github.com>
2024-09-19 16:52:21 -0500

    fix(archunitsonar): Fix UpgradeAction for jacoco merged report adding arch unit issues to sonar (#549)
    * fix(archunitsonar): Fix UpgradeAction for jacoco merged report adding arch unit issues to sonar

--- src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2023M10D02SonarRules.java
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2023M10D02SonarRules.java
@@ -1,6 +1,7 @@
 package co.com.bancolombia.factory.upgrades.actions;
 
 import co.com.bancolombia.Constants;
+import co.com.bancolombia.exceptions.InvalidStateException;
 import co.com.bancolombia.factory.ModuleBuilder;
 import co.com.bancolombia.factory.upgrades.UpdateUtils;
 import co.com.bancolombia.factory.upgrades.UpgradeAction;
@@ -11,15 +12,22 @@ public class UpgradeY2023M10D02SonarRules implements UpgradeAction {
       "\n" + "        property \"sonar.externalIssuesReportPaths\", \"build/issues.json\"";
   public static final String ISSUES_JSON = "issues.json";
   public static final String JACOCO_TEST_REPORT_XML = "jacocoTestReport.xml\"";
+  public static final String JACOCO_MERGED_REPORT_XML = "jacocoMergedReport.xml\"";
 
   @Override
   @SneakyThrows
   public boolean up(ModuleBuilder builder) {
-    return builder.updateFile(
-        Constants.MainFiles.BUILD_GRADLE,
-        content ->
-            UpdateUtils.insertAfterMatch(
-                content, JACOCO_TEST_REPORT_XML, ISSUES_JSON, CONCAT_VALUE));
+    return builder.updateFile(Constants.MainFiles.BUILD_GRADLE, this::insert);
+  }
+
+  private String insert(String content) {
+    try {
+      return UpdateUtils.insertAfterMatch(
+          content, JACOCO_TEST_REPORT_XML, ISSUES_JSON, CONCAT_VALUE);
+    } catch (InvalidStateException ignored) {
+      return UpdateUtils.insertAfterMatch(
+          content, JACOCO_MERGED_REPORT_XML, ISSUES_JSON, CONCAT_VALUE);
+    }
   }
 
   @Override
