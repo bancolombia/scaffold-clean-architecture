ca412db2ea8f8ed5efcd34013bed93253dcf8299
luisgomez29 <luisgaleano002@gmail.com>
2024-11-16 18:39:58 -0500

    feat(gradle): fix UpgradeAction for gradle 8.11

--- src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2024M11D16Gradle.java
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2024M11D16Gradle.java
@@ -8,17 +8,33 @@ import co.com.bancolombia.factory.upgrades.UpgradeAction;
 import lombok.SneakyThrows;
 
 public class UpgradeY2024M11D16Gradle implements UpgradeAction {
+  private static final String JAVA_BLOCK_CONFIG =
+      "java {\n        {{sourceCompatibilityLoaded}}\n    }";
 
   @Override
   @SneakyThrows
   public boolean up(ModuleBuilder builder) {
     return builder.updateFile(
         MAIN_GRADLE,
-        content ->
-            UpdateUtils.replace(
-                content,
-                "sourceCompatibility = JavaVersion.VERSION_17",
-                "\n\tjava {\n" + "\t\tsourceCompatibility = JavaVersion.VERSION_17\n" + "\t}"));
+        content -> {
+          String sourceCompatibilityLoaded =
+              builder
+                  .findExpressions(
+                      MAIN_GRADLE, "sourceCompatibility = JavaVersion.VERSION_(\\d{2})")
+                  .stream()
+                  .findFirst()
+                  .orElse(null);
+
+          if (sourceCompatibilityLoaded == null) {
+            sourceCompatibilityLoaded = "sourceCompatibility = JavaVersion.{{javaVersion}}";
+          }
+
+          return UpdateUtils.replace(
+              content,
+              sourceCompatibilityLoaded,
+              JAVA_BLOCK_CONFIG.replace(
+                  "{{sourceCompatibilityLoaded}}", sourceCompatibilityLoaded));
+        });
   }
 
   @Override
