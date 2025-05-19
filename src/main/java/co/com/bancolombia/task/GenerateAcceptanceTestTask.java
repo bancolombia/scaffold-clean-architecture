9e4c085257c767e05c7b1a2d5011a5dc993a2116
lugomez <lugomez@bancolombia.com.co>
2025-05-19 17:02:27 -0500

    feat(gradle): enhance GenerateAcceptanceTestTask with projectPath and moduleNames properties

--- src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
+++ src/main/java/co/com/bancolombia/task/GenerateAcceptanceTestTask.java
@@ -4,6 +4,10 @@ import co.com.bancolombia.exceptions.CleanException;
 import co.com.bancolombia.task.annotations.CATask;
 import java.io.IOException;
 import java.util.ArrayList;
+import lombok.Getter;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.api.tasks.Input;
 
 @CATask(
     name = "generateAcceptanceTest",
@@ -11,20 +15,28 @@ import java.util.ArrayList;
     description = "Generate subproject by karate framework in deployment layer")
 public class GenerateAcceptanceTestTask extends AbstractResolvableTypeTask {
 
+  @Input @Getter private final Property<String> projectPath;
+  @Input @Getter private final SetProperty<String> moduleNames;
+
   public GenerateAcceptanceTestTask() {
     notCompatibleWithConfigurationCache("This task performs validations that should always run");
+
+    this.projectPath = getProject().getObjects().property(String.class);
+    this.projectPath.set(getProject().getProjectDir().getPath());
+
+    this.moduleNames = getProject().getObjects().setProperty(String.class);
+    this.moduleNames.set(getProject().getChildProjects().keySet());
   }
 
   @Override
   public void execute() throws IOException, CleanException {
     super.execute();
-    builder.runTask(
-        "wrapper", getProject().getProjectDir().getPath().concat("/deployment/" + name));
+    builder.runTask("wrapper", projectPath.get().concat("/deployment/" + name));
   }
 
   @Override
   protected void prepareParams() {
-    var modules = new ArrayList<>(getProject().getChildProjects().keySet());
+    var modules = new ArrayList<>(moduleNames.get());
 
     builder.addParam(
         "task-param-exist-api-rest",
