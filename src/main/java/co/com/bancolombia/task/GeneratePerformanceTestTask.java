6dc0d222cdae4ed010d988a3c77b2deccd092467
lugomez <lugomez@bancolombia.com.co>
2025-05-21 15:14:33 -0500

    feat(gradle): add moduleNames property to GeneratePerformanceTestTask

--- src/main/java/co/com/bancolombia/task/GeneratePerformanceTestTask.java
+++ src/main/java/co/com/bancolombia/task/GeneratePerformanceTestTask.java
@@ -2,16 +2,25 @@ package co.com.bancolombia.task;
 
 import co.com.bancolombia.task.annotations.CATask;
 import java.util.ArrayList;
+import lombok.Getter;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.api.tasks.Input;
 
 @CATask(
     name = "generatePerformanceTest",
     shortcut = "gpt",
     description = "Generate performance test")
 public class GeneratePerformanceTestTask extends AbstractResolvableTypeTask {
+  @Input @Getter private final SetProperty<String> moduleNames;
+
+  public GeneratePerformanceTestTask() {
+    this.moduleNames = getProject().getObjects().setProperty(String.class);
+    this.moduleNames.set(getProject().getChildProjects().keySet());
+  }
 
   @Override
   protected void prepareParams() {
-    var modules = new ArrayList<>(getProject().getChildProjects().keySet());
+    var modules = new ArrayList<>(moduleNames.get());
 
     builder.addParam(
         "task-param-exist-api-rest",
