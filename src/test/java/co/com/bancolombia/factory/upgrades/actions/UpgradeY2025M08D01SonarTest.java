1958a4747ce5fb0d3ddbd69f72617d54fba9f449
lugomez <lugomez@bancolombia.com.co>
2025-08-01 11:10:10 -0500

    build: update dependencies

--- src/test/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2025M08D01SonarTest.java
+++ src/test/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2025M08D01SonarTest.java
@@ -18,8 +18,11 @@ import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class UpgradeY2025M08D01SonarTest {
   @Mock private Project project;
   @Mock private Logger logger;
