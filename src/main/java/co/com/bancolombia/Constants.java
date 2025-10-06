a7094f030d3a0871a356c02cc470d303f8483fc0
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2025-10-06 12:07:56 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -18,7 +18,7 @@ public final class Constants {
   public static final String REACTIVE_COMMONS_VERSION = "5.5.0";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.13.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.33.1";
+  public static final String AWS_BOM_VERSION = "2.34.0";
   public static final String COMMONS_JMS_VERSION = "2.4.5";
   public static final String GRAPHQL_KICKSTART_VERSION = "15.1.0";
   public static final String ARCH_UNIT_VERSION = "1.4.1";
@@ -32,8 +32,8 @@ public final class Constants {
   public static final String SONAR_VERSION = "6.3.1.5724";
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String PLUGIN_VERSION = "3.26.1";
-  public static final String DEPENDENCY_CHECK_VERSION = "12.1.3";
-  public static final String PITEST_VERSION = "1.19.0-rc.1";
+  public static final String DEPENDENCY_CHECK_VERSION = "12.1.6";
+  public static final String PITEST_VERSION = "1.19.0-rc.2";
   // custom
   public static final String GRADLE_WRAPPER_VERSION = "8.14.3";
 
