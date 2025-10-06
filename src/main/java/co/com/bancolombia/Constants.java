62b75144b6e6f3b9cec117b94a0067319a7b9991
lugomez <lugomez@bancolombia.com.co>
2025-10-06 11:16:31 -0500

    fix: remove deprecated GRAPHQL_KICKSTART_VERSION from Constants.java and sh_dependencies.json

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -20,8 +20,6 @@ public final class Constants {
   public static final String BLOCK_HOUND_VERSION = "1.0.14.RELEASE";
   public static final String AWS_BOM_VERSION = "2.35.0";
   public static final String COMMONS_JMS_VERSION = "2.5.0";
-  @Deprecated
-  public static final String GRAPHQL_KICKSTART_VERSION = "15.1.0";
   public static final String ARCH_UNIT_VERSION = "1.4.1";
   public static final String OKHTTP_VERSION = "5.1.0";
   public static final String RESILIENCE_4J_VERSION = "2.3.0";
