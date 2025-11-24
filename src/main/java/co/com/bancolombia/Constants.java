bbe7fb9a53b55b7f144297eba2d90cde93101d16
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2025-11-24 12:09:04 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -13,28 +13,28 @@ public final class Constants {
   public static final String PATH_GRAPHQL = "/graphql";
   // dependencies
   public static final String SECRETS_VERSION = "4.4.36";
-  public static final String SPRING_BOOT_VERSION = "3.5.7";
+  public static final String SPRING_BOOT_VERSION = "4.0.0";
   public static final String LOMBOK_VERSION = "1.18.42";
   public static final String REACTIVE_COMMONS_VERSION = "6.1.0";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.15.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.38.7";
+  public static final String AWS_BOM_VERSION = "2.39.2";
   public static final String COMMONS_JMS_VERSION = "2.5.3";
   public static final String ARCH_UNIT_VERSION = "1.4.1";
-  public static final String OKHTTP_VERSION = "5.3.1";
+  public static final String OKHTTP_VERSION = "5.3.2";
   public static final String RESILIENCE_4J_VERSION = "2.3.0";
   public static final String BIN_STASH_VERSION = "1.3.2";
-  public static final String SPRING_DOC_OPENAPI_VERSION = "2.8.14";
+  public static final String SPRING_DOC_OPENAPI_VERSION = "3.0.0";
   public static final String CLOUD_EVENTS_VERSION = "4.0.1";
   // gradle plugins
   public static final String JACOCO_VERSION = "0.8.14";
-  public static final String SONAR_VERSION = "7.0.1.6134";
+  public static final String SONAR_VERSION = "7.1.0.6387";
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String PLUGIN_VERSION = "3.27.0";
   public static final String DEPENDENCY_CHECK_VERSION = "12.1.9";
   public static final String PITEST_VERSION = "1.19.0-rc.2";
   // custom
-  public static final String GRADLE_WRAPPER_VERSION = "8.14.3";
+  public static final String GRADLE_WRAPPER_VERSION = "9.2.1";
 
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
   public static class MainFiles {
