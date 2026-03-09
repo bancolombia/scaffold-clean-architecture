aa6d6afa079d2806080961f165b2a9a65b6b4370
luisgomez29 <luisgaleano002@gmail.com>
2026-03-09 11:05:54 -0500

    build(deps): update dependencies

--- src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2025M12D12SpringBoot4.java
+++ src/main/java/co/com/bancolombia/factory/upgrades/actions/UpgradeY2025M12D12SpringBoot4.java
@@ -159,9 +159,17 @@ public class UpgradeY2025M12D12SpringBoot4 implements UpgradeAction {
                         "org.springframework.boot.amqp.autoconfigure");
 
                 // Jackson package changes (com.fasterxml moved to tools)
-                updatedContent = UpdateUtils.replace(updatedContent, "com.fasterxml.jackson.core", "tools.jackson.core");
-                updatedContent = UpdateUtils.replace(updatedContent, "com.fasterxml.jackson.databind", "tools.jackson.databind");
-                updatedContent = UpdateUtils.replace(updatedContent, "tools.jackson.core:jackson-annotations", "com.fasterxml.jackson.core:jackson-annotations");
+                updatedContent =
+                    UpdateUtils.replace(
+                        updatedContent, "com.fasterxml.jackson.core", "tools.jackson.core");
+                updatedContent =
+                    UpdateUtils.replace(
+                        updatedContent, "com.fasterxml.jackson.databind", "tools.jackson.databind");
+                updatedContent =
+                    UpdateUtils.replace(
+                        updatedContent,
+                        "tools.jackson.core:jackson-annotations",
+                        "com.fasterxml.jackson.core:jackson-annotations");
                 updatedContent =
                     UpdateUtils.replace(
                         updatedContent, "JsonProcessingException", "JacksonException");
