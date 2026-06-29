d0fdc1a1d22ed6ac03c723aee1f07bc202fe9588
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2026-06-29 13:32:09 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -15,10 +15,10 @@ public final class Constants {
   public static final String SECRETS_VERSION = "4.5.2";
   public static final String SPRING_BOOT_VERSION = "4.1.0";
   public static final String LOMBOK_VERSION = "1.18.46";
-  public static final String REACTIVE_COMMONS_VERSION = "7.2.1";
+  public static final String REACTIVE_COMMONS_VERSION = "7.2.2";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.17.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.46.8";
+  public static final String AWS_BOM_VERSION = "2.46.17";
   public static final String COMMONS_JMS_VERSION = "3.1.1";
   public static final String ARCH_UNIT_VERSION = "1.4.2";
   public static final String OKHTTP_VERSION = "5.4.0";
@@ -28,8 +28,8 @@ public final class Constants {
   public static final String CLOUD_EVENTS_VERSION = "4.1.1";
   public static final String REACTOR_KAFKA_VERSION = "1.3.25";
   public static final String SPRING_AI_VERSION = "2.0.0-RC1";
-  public static final String SPRING_MCP_SECURITY_VERSION = "0.1.12";
-  public static final String PITEST_VERSION = "1.25.4";
+  public static final String SPRING_MCP_SECURITY_VERSION = "0.1.13";
+  public static final String PITEST_VERSION = "1.25.5";
   public static final String PITEST_HISTORY_VERSION = "0.0.1";
   public static final String PITEST_JUNIT5_VERSION = "1.2.3";
   // gradle plugins
@@ -40,7 +40,7 @@ public final class Constants {
   public static final String DEPENDENCY_CHECK_VERSION = "12.2.2";
   public static final String GRADLE_PITEST_VERSION = "1.19.0";
   // custom
-  public static final String GRADLE_WRAPPER_VERSION = "9.5.1";
+  public static final String GRADLE_WRAPPER_VERSION = "9.6.1";
 
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
   public static class MainFiles {
