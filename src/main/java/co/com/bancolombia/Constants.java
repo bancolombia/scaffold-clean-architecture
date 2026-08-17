7696332e58acf45c67d2a4c288b2ffdad5cb30db
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2026-08-17 12:08:34 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -18,10 +18,10 @@ public final class Constants {
   public static final String REACTIVE_COMMONS_VERSION = "7.3.0";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.17.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.52.0";
+  public static final String AWS_BOM_VERSION = "2.53.1";
   public static final String COMMONS_JMS_VERSION = "3.1.3";
   public static final String ARCH_UNIT_VERSION = "1.5.0";
-  public static final String OKHTTP_VERSION = "5.4.0";
+  public static final String OKHTTP_VERSION = "5.5.0";
   public static final String RESILIENCE_4J_VERSION = "2.4.0";
   public static final String BIN_STASH_VERSION = "2.1.2";
   public static final String SPRING_DOC_OPENAPI_VERSION = "3.1.0";
@@ -29,7 +29,7 @@ public final class Constants {
   public static final String REACTOR_KAFKA_VERSION = "1.3.25";
   public static final String SPRING_AI_VERSION = "2.0.0";
   public static final String SPRING_MCP_SECURITY_VERSION = "0.1.14";
-  public static final String JACKSON_VERSION = "3.2.1";
+  public static final String JACKSON_VERSION = "3.2.2";
   public static final String PITEST_VERSION = "1.25.9";
   public static final String PITEST_HISTORY_VERSION = "0.0.1";
   public static final String PITEST_JUNIT5_VERSION = "1.2.3";
@@ -40,8 +40,8 @@ public final class Constants {
   public static final String PLUGIN_VERSION = "4.6.0";
   public static final String DEPENDENCY_CHECK_VERSION = "13.0.0";
   public static final String GRADLE_PITEST_VERSION = "1.19.0";
-  public static final String GRPC_VERSION = "1.81.0";
-  public static final String PROTOBUF_VERSION = "4.33.4";
+  public static final String GRPC_VERSION = "1.83.1";
+  public static final String PROTOBUF_VERSION = "4.35.1";
   public static final String PROTOBUF_PLUGIN_VERSION = "4.6.0";
   // custom
   public static final String GRADLE_WRAPPER_VERSION = "9.7.0";
