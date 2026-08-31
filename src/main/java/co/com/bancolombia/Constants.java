8fb2a7eef01a5c335b0416490041e30f9e1e25e5
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2026-08-31 12:17:08 +0000

    fix(deps): update dependencies

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
   public static final String REACTIVE_COMMONS_VERSION = "7.3.1";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.17.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.54.1";
+  public static final String AWS_BOM_VERSION = "2.54.7";
   public static final String COMMONS_JMS_VERSION = "3.1.4";
   public static final String ARCH_UNIT_VERSION = "1.5.0";
   public static final String OKHTTP_VERSION = "5.5.0";
@@ -31,7 +31,7 @@ public final class Constants {
   public static final String SPRING_AI_VERSION = "2.0.1";
   public static final String SPRING_MCP_SECURITY_VERSION = "0.1.14";
   public static final String JACKSON_VERSION = "3.2.2";
-  public static final String PITEST_VERSION = "1.25.9";
+  public static final String PITEST_VERSION = "1.30.0";
   public static final String PITEST_HISTORY_VERSION = "0.0.1";
   public static final String PITEST_JUNIT5_VERSION = "1.2.3";
   public static final String GRPC_VERSION = "1.83.1";
