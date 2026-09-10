72351e1a831253e786e16b74d365a6ba5e05ea8f
Juan Campuzano <diegocampuzano62@gmail.com>
2026-09-10 15:54:41 -0500

    fix(deps): update dependecies versions

--- src/main/java/co/com/bancolombia/Constants.java
+++ src/main/java/co/com/bancolombia/Constants.java
@@ -15,26 +15,26 @@ public final class Constants {
   // Dependencies
   public static final String SECRETS_VERSION = "4.5.3";
   public static final String SPRING_BOOT_VERSION = "4.1.1";
-  public static final String LOMBOK_VERSION = "1.18.46";
+  public static final String LOMBOK_VERSION = "1.18.48";
   public static final String REACTIVE_COMMONS_VERSION = "7.3.1";
   public static final String REACTIVE_COMMONS_MAPPER_VERSION = "0.1.0";
   public static final String BLOCK_HOUND_VERSION = "1.0.17.RELEASE";
-  public static final String AWS_BOM_VERSION = "2.54.1";
+  public static final String AWS_BOM_VERSION = "2.54.15";
   public static final String COMMONS_JMS_VERSION = "3.1.4";
   public static final String ARCH_UNIT_VERSION = "1.5.0";
   public static final String OKHTTP_VERSION = "5.5.0";
   public static final String RESILIENCE_4J_VERSION = "2.4.0";
-  public static final String BIN_STASH_VERSION = "2.1.3";
-  public static final String SPRING_DOC_OPENAPI_VERSION = "3.1.0";
+  public static final String BIN_STASH_VERSION = "2.1.4";
+  public static final String SPRING_DOC_OPENAPI_VERSION = "3.1.1";
   public static final String CLOUD_EVENTS_VERSION = "5.0.0";
   public static final String REACTOR_KAFKA_VERSION = "1.3.25";
   public static final String SPRING_AI_VERSION = "2.0.1";
   public static final String SPRING_MCP_SECURITY_VERSION = "0.1.14";
   public static final String JACKSON_VERSION = "3.2.2";
-  public static final String PITEST_VERSION = "1.25.9";
+  public static final String PITEST_VERSION = "1.30.0";
   public static final String PITEST_HISTORY_VERSION = "0.0.1";
   public static final String PITEST_JUNIT5_VERSION = "1.2.3";
-  public static final String GRPC_VERSION = "1.83.1";
+  public static final String GRPC_VERSION = "1.84.0";
 
   /**
    * Must match the protobuf-java version managed by the Spring Boot BOM ({@link
@@ -46,11 +46,11 @@ public final class Constants {
   // Gradle plugins
   public static final String PLUGIN_VERSION = "4.6.1";
   public static final String JACOCO_VERSION = "0.8.15";
-  public static final String SONAR_VERSION = "7.4.0.8496";
+  public static final String SONAR_VERSION = "7.5.0.8588";
   public static final String COBERTURA_VERSION = "4.0.0";
   public static final String DEPENDENCY_CHECK_VERSION = "13.0.0";
   public static final String GRADLE_PITEST_VERSION = "1.19.0";
-  public static final String PROTOBUF_PLUGIN_VERSION = "4.6.1";
+  public static final String GRADLE_PROTOBUF_VERSION = "0.10.0";
 
   // Custom
   public static final String GRADLE_WRAPPER_VERSION = "9.7.1";
